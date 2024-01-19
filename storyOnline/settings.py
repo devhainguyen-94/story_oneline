06264fa0b3a162e0860449c3c0817c01f6b4d9dd
hainh3 <hainh3@smartosc.com>
2024-01-19 18:04:03 +0700

    api_branch

--- storyOnline/settings.py
+++ storyOnline/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'storyOnline.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'render'  # Here
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

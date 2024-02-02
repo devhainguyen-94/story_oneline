49ddd86d550a2dffd4b03da275c5e002d98d3fcc
hainh3 <hainh3@smartosc.com>
2024-02-02 17:42:30 +0700

    migration for models

--- main/models.py
+++ main/models.py
@@ -1,27 +1,44 @@
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
+from django.conf import settings
 class Role(models.Model):
     name = models.TextField()
     role_name = models.TextField()
-    created_by = models.ForeignKey('auth.User', related_name='user', on_delete=models.CASCADE)
+    # created_by = models.ForeignKey(User, related_name='cre', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     class Meta:
         ordering = ['created_at']
+class CustomUser(AbstractUser):
+    # Add custom fields here, if needed
+    # ROLES = [
+    #     ('admin', 'Administrator'),
+    #     ('editor', 'Editor'),
+    #     ('viewer', 'Viewer'),
+    # ]
+
+    role = models.ForeignKey(Role, related_name='role', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.username
+
+
 class Category(models.Model):
     name = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey('auth.User', related_name='user', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    # auth =  models.ForeignKey(User, on_delete=models.CASCADE)
+    auth =  models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
     image = models.ImageField()
     category = models.ManyToManyField(Category)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey('auth.User', related_name='user', on_delete=models.CASCADE)
+    # created_by = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
 class Chapter(models.Model):
     number_chapter = models.IntegerField(default=0)
     content = models.TextField()
@@ -31,4 +48,4 @@ class Chapter(models.Model):
     book = models.ForeignKey(Book, on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey('auth.User', related_name='user', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='created_by', on_delete=models.CASCADE)

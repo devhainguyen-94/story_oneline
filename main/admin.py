786635106aea004bcbfa4815bded83d053f486e1
hainh <master.dkny1102@gmail.com>
2024-02-07 00:18:25 +0700

    add register

--- main/admin.py
+++ main/admin.py
@@ -6,9 +6,9 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
-#    list_display = (
-#         'username', 'email', 'first_name', 'last_name','role'
-#         )
+    list_display = (
+        'username', 'email', 'first_name', 'last_name','role',
+        )
     add_fieldsets = (
         (None, {
             'fields': ('username', 'password1', 'password2')
--- main/serializers.py
+++ main/serializers.py
@@ -4,15 +4,21 @@ from rest_framework import serializers
 from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
+   
     class Meta:
         model = CustomUser
         fields = ['username', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
+        ROLES = [
+        ('admin', 'Administrator'),
+        ('editor', 'Editor'),
+        ('viewer', 'Viewer'),
+        ]
         user = CustomUser(
             username=validated_data['username'],
-            role=('viewer', 'Viewer')
+            role=ROLES[2][0]
         )
         user.set_password(validated_data['password'])
         user.save()

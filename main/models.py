3979e15f2967cba146f8591e214ddae276b33231
hainh3 <hainh3@smartosc.com>
2025-02-12 18:14:25 +0700

    updpate db

--- main/models.py
+++ main/models.py
@@ -38,14 +38,33 @@ class Book(models.Model):
     category = models.ManyToManyField(Category)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # created_by = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='books', on_delete=models.CASCADE)
+
 class Chapter(models.Model):
     number_chapter = models.IntegerField(default=0)
     content = models.TextField()
     image = models.ImageField(upload_to='images/chapter/')
     title = models.CharField(max_length=200)
     description = models.TextField()
+    audio_id = models.ForeignKey('Audio', on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(User, related_name='created_by', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='chapters', on_delete=models.CASCADE)
+class Cluster(models.Model):
+    cloud_name = models.TextField()
+    api_key = models.TextField()
+    api_secret = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, related_name='clusters', on_delete=models.CASCADE)
+class Audio(models.Model):
+    Type = [('image', 'Image'),
+            ('audio', 'Audio')]
+    title = models.CharField(max_length=200)
+    link = models.TextField()
+    cluster_id = models.ForeignKey(Cluster, on_delete=models.CASCADE)
+    type =   models.CharField(max_length=20, choices=Type, default=Type[0][0])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, related_name='audios', on_delete=models.CASCADE)
